35821a877fa7ef6f5c936f8b16e3c810c4b4ad67
Wiktor Józwiak <61411400+ReferTV@users.noreply.github.com>
2022-10-12 17:49:31 +0200

    remove bg

--- pages/index.vue
+++ pages/index.vue
@@ -97,21 +97,4 @@ export default {
 </script>
 
 <style>
-#app {
-	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-	background-size: 200% 200%;
-	animation: gradient 15s ease infinite;
-}
-
-@keyframes gradient {
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-	}
-}
 </style>
