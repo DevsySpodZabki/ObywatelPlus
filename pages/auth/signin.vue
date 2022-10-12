309c2b54f542913ec41d07b71e53a78133f81d44
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-10-12 19:15:34 +0200

    fix stepper

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,66 +1,65 @@
 <template>
   <v-container>
-    <v-stepper v-model="e1">
-      <v-stepper-header>
-        <v-stepper-step
-          :complete="e1 > 1"
-          step="1"
-        >
-          Name of step 1
-        </v-stepper-step>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="8" lg="6">
+        <v-btn class="mb-2 pl-2 rounded-lg" to="/" text>
+          <v-icon>mdi-arrow-left-thin</v-icon>
+          Powrót do strony głównej
+        </v-btn>
+        <v-stepper v-model="e1">
+          <v-stepper-header>
+            <v-stepper-step
+              :complete="e1 > 1"
+              step="1"
+            >
+              Wprowadź numer telefonu
+            </v-stepper-step>
 
-        <v-divider />
+            <v-divider />
 
-        <v-stepper-step
-          :complete="e1 > 2"
-          step="2"
-        >
-          Name of step 2
-        </v-stepper-step>
+            <v-stepper-step
+              :complete="e1 > 2"
+              step="2"
+            >
+              Wpisz kod weryfikacyjny
+            </v-stepper-step>
 
-        <v-divider />
+          </v-stepper-header>
 
-        <v-stepper-step step="3">
-          Name of step 3
-        </v-stepper-step>
-      </v-stepper-header>
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <VuePhoneNumberInput v-model="number" :translations="translations" dark  class="mb-5"/>
 
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <VuePhoneNumberInput v-model="number" :translations="translations" dark  class="mb-5"/>
+              <v-btn
+              class="rounded-lg"
+                color="primary"
+                @click="e1 = 2;login()"
+              >
+                Dalej
+              </v-btn>
+            </v-stepper-content>
 
-          <v-btn
-          class="rounded-lg"
-            color="primary"
-            @click="e1 = 2;login()"
-          >
-            Dalej
-          </v-btn>
+            <v-stepper-content step="2">
+              <v-card
+                class="mb-12"
+                color="grey lighten-1"
+                height="200px"
+              />
 
-          <v-btn text class="rounded-lg">
-            Cancel
-          </v-btn>
-        </v-stepper-content>
+              <v-btn
+                color="primary"
+              >
+                Dalej
+              </v-btn>
 
-        <v-stepper-content step="2">
-          <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="200px"
-          />
-
-          <v-btn
-            color="primary"
-          >
-            Continue
-          </v-btn>
-
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
+              <v-btn text @click="e1=1">
+                Cofnij się
+              </v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-col>
+    </v-row>
     <div
       id="recaptcha-container"
       class="justify-center flex"
