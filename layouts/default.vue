cfb73af28fd62dbc3696e0440dd048be218fc3f3
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-10-13 21:45:58 +0200

    some changes (#11)
    some changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,13 +5,28 @@
     <v-main>
       <nuxt v-if="!$slots.default" />
       <slot />
+      <div
+      id="recaptcha-container"
+      class="justify-center flex"
+    />
     </v-main>
     <AppFooter />
   </v-app>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-  name: 'DefaultLayout'
+  name: 'DefaultLayout',
+  mounted(){
+    Vue.prototype.appVerifier = new this.$fireModule.auth.RecaptchaVerifier('recaptcha-container', {
+      size: 'invisible',
+      callback: (response) => {
+        console.log('captcha works')
+      }
+    })
+    Vue.prototype.appVerifier.render()
+  }
 }
 </script>
