4ba272e7bc2479e59dd3d233aecd33ff8f68ac69
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-10-13 19:46:20 +0200

    remove sign in and panel page (#10)
    * remove sign in and panel page

--- components/app-bar.vue
+++ components/app-bar.vue
@@ -1,35 +1,31 @@
 <template>
-  <v-app-bar fixed light app>
-    <v-toolbar-title>
-      <router-link to="/">
-        <v-btn class="mx-0">
+  <div>
+    <v-app-bar fixed light app>
+      <nuxt-link to="/" style="text-decoration: none; color: inherit;">
+        <v-toolbar-title class="mr-3 ml-3">
           Obywatel Plus
-        </v-btn>
-      </router-link>
-    </v-toolbar-title>
-    <v-spacer />
-    <v-menu left bottom>
-      <template #activator="{ on, attrs }">
-        <v-btn
-          icon
-          aria-label="theme"
-          v-bind="attrs"
-          @click="toggleDarkTheme"
-          v-on="on"
-        >
-          <v-icon v-if="$vuetify.theme.dark">
-            mdi-white-balance-sunny
-          </v-icon>
-          <v-icon v-if="!$vuetify.theme.dark">
-            mdi-weather-night
-          </v-icon>
-        </v-btn>
-        <v-btn v-if="!loggedIn" text to="/auth/signin">
-          Zaloguj się
-        </v-btn>
-        <v-btn v-else text to="/panel">
-          Przejdź do panelu
-        </v-btn>
+        </v-toolbar-title>
+      </nuxt-link>
+      <v-spacer />
+      <v-menu left bottom>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            icon
+            aria-label="theme"
+            v-bind="attrs"
+            @click="toggleDarkTheme"
+            v-on="on"
+          >
+            <v-icon v-if="$vuetify.theme.dark">
+              mdi-white-balance-sunny
+            </v-icon>
+            <v-icon v-if="!$vuetify.theme.dark">
+              mdi-weather-night
+            </v-icon>
+          </v-btn>
+        </template>
+      </v-menu>
+      <v-toolbar-items>
         <v-btn color="accent" text>
           <v-icon>
             mdi-map-marker-radius
@@ -46,9 +42,39 @@
             Zbiórki & budżety obywatelskie
           </span>
         </v-btn>
-      </template>
-    </v-menu>
-  </v-app-bar>
+        <v-dialog
+          v-if="!loggedIn"
+          width="500"
+        >
+          <template #activator="{ on, attrs }">
+            <v-btn
+              color="red lighten-2"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              Zaloguj się
+            </v-btn>
+          </template>
+          <v-card class="rounded-lg">
+            <v-card-title class="text-h5">
+              Zaloguj się
+            </v-card-title>
+            <v-card-text>
+              <App-signin />
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+        <v-btn v-else text @click="logout">
+          Wyloguj się
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <div
+      id="recaptcha-container"
+      class="justify-center flex"
+    />
+  </div>
 </template>
 
 <script>
@@ -65,6 +91,10 @@ export default {
     toggleDarkTheme () {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark
       localStorage.setItem('dark', this.$vuetify.theme.dark.toString())
+    },
+    logout () {
+      this.$fire.auth.signOut()
+      this.$router.push('/')
     }
   }
 }
